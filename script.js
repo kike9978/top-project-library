7777b0d6240c916cc0ffe4cfb42b62beb61a4353
Kike Mendoza <kike.mendoza9978@gmail.com>
2023-12-29 17:56:53 -0600

    Change constructor functions to classes

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const data = [
   { title: "Como cautivar", author: " L Zavaleta", pages: 430, isRead: false },
 ];
 
-const myLibrary = []
-
 
 
 class Book {
@@ -21,9 +19,9 @@ class Book {
   delete(e) {
     const bookID = e.target.parentNode.getAttribute("data-book")
     console.log(e)
-    console.log(myLibrary.findIndex(book => book.author === this.author))
-    myLibrary.splice(myLibrary.findIndex(book => book.author === this.author), 1)
-    console.log(myLibrary)
+    console.log(Library.myLibrary.findIndex(book => book.author === this.author))
+    Library.myLibrary.splice(Library.myLibrary.findIndex(book => book.author === this.author), 1)
+    console.log(Library.myLibrary)
     const rowToDelete = document.querySelector(`tr[data-book=${bookID}]`)
     console.log(bookID)
     rowToDelete.remove()
@@ -31,109 +29,120 @@ class Book {
 
   changeReadStatus(e) {
     const bookID = e.target.parentNode.getAttribute("data-book")
-    const arrayIndex = myLibrary.findIndex(book => { return formatBookID(book) === bookID })
-    let readStatus = myLibrary[arrayIndex].isRead
-    myLibrary[arrayIndex].isRead = !readStatus
-    readStatus = myLibrary[arrayIndex].isReadd
+    const arrayIndex = Library.myLibrary.findIndex(book => { return formatBookID(book) === bookID })
+    let readStatus = Library.myLibrary[arrayIndex].isRead
+    Library.myLibrary[arrayIndex].isRead = !readStatus
+    readStatus = Library.myLibrary[arrayIndex].isRead
     const cellToEdit = document.querySelector(`tr[data-book=${bookID}] td[data-cell=isRead]`)
     cellToEdit.innerText = readStatus ? "Leído" : "No leído"
   }
 
 }
 
-data.map(book => {
-  myLibrary.push(new Book(book.title, book.author, book.pages, book.isRead))
-})
+class Library {
+  static myLibrary = []
+  static generateLibrary() {
+    data.map(book => {
+      Library.myLibrary.push(new Book(book.title, book.author, book.pages, book.isRead))
+    })
+  }
+
+  static addBookToLibrary(e) {
+    e.preventDefault()
+    const formData = new FormData(form)
+    modal.close();
+    const entries = Object.fromEntries(formData)
+    console.log(entries)
+    const book = new Book(entries.title, entries.author, entries.pages, entries.isRead)
+    Library.myLibrary.push(book)
+    UIRenderer.generateTable()
+  }
+}
+
+Library.generateLibrary()
 
 const form = document.querySelector("form")
 
-function addBookToLibrary(e) {
-  e.preventDefault()
-  const formData = new FormData(form)
-  modal.close();
-  const entries = Object.fromEntries(formData)
-  console.log(entries)
-  const book = new Book(entries.title, entries.author, entries.pages, entries.isRead)
-  myLibrary.push(book)
-  generateTable()
-}
-
-function generateRow() {
-
-}
-
 const tbody = document.querySelector("tbody")
 
-function generateTable() {
-  tbody.innerHTML = ""
+class UIRenderer {
+  static generateTable() {
+    tbody.innerHTML = ""
 
-  myLibrary.forEach(book => {
+    Library.myLibrary.forEach(book => {
 
-    const tr = document.createElement("tr")
+      const tr = document.createElement("tr")
 
-    const readStatusBtn = document.createElement("button")
-    readStatusBtn.innerText = "Cambiar estatus de leído";
-    readStatusBtn.addEventListener("click", e => Book.prototype.changeReadStatus(e))
-    const editBtn = document.createElement("button")
-    editBtn.innerText = "Editar";
-    editBtn.addEventListener("click", e => {
-      handleOpenModal(e);
+      const readStatusBtn = document.createElement("button")
+      readStatusBtn.innerText = "Cambiar estatus de leído";
+      readStatusBtn.addEventListener("click", e => Book.prototype.changeReadStatus(e))
+      const editBtn = document.createElement("button")
+      editBtn.innerText = "Editar";
+      editBtn.addEventListener("click", e => {
+        UIRenderer.handleOpenModal(e);
+      })
+
+      const deleteBtn = document.createElement("button")
+      deleteBtn.innerText = "Eliminar";
+      deleteBtn.addEventListener("click", (e) => Book.prototype.delete(e))
+
+      tr.setAttribute("data-book", formatBookID(book))
+      const cellTitle = document.createElement("td")
+      const cellAuthor = document.createElement("td")
+      const cellPages = document.createElement("td")
+      const cellIsRead = document.createElement("td")
+      cellIsRead.setAttribute("data-cell", "isRead")
+      cellTitle.innerText = book.title;
+      cellAuthor.innerText = book.author;
+      cellPages.innerText = book.pages;
+      cellIsRead.innerText = book.isRead ? "Leído" : "No leído";
+      tr.appendChild(cellTitle)
+      tr.appendChild(cellAuthor)
+      tr.appendChild(cellPages)
+      tr.appendChild(cellIsRead)
+      tr.appendChild(readStatusBtn)
+      tr.appendChild(editBtn)
+      tr.appendChild(deleteBtn)
+
+      tbody.appendChild(tr)
     })
+  }
 
-    const deleteBtn = document.createElement("button")
-    deleteBtn.innerText = "Eliminar";
-    deleteBtn.addEventListener("click", (e) => Book.prototype.delete(e))
+  static handleOpenModal(e) {
+    e.preventDefault();
+    form.reset()
+    modal.showModal();
+  }
 
-    tr.setAttribute("data-book", formatBookID(book))
-    const cellTitle = document.createElement("td")
-    const cellAuthor = document.createElement("td")
-    const cellPages = document.createElement("td")
-    const cellIsRead = document.createElement("td")
-    cellIsRead.setAttribute("data-cell", "isRead")
-    cellTitle.innerText = book.title;
-    cellAuthor.innerText = book.author;
-    cellPages.innerText = book.pages;
-    cellIsRead.innerText = book.isRead ? "Leído" : "No leído";
-    tr.appendChild(cellTitle)
-    tr.appendChild(cellAuthor)
-    tr.appendChild(cellPages)
-    tr.appendChild(cellIsRead)
-    tr.appendChild(readStatusBtn)
-    tr.appendChild(editBtn)
-    tr.appendChild(deleteBtn)
+  static handleAddBookBtn() {
+    modal.close()
+  }
 
-    tbody.appendChild(tr)
-  })
+  static handleCloseModal(e) {
+    e.preventDefault
+    modal.close()
+  }
 }
+
+
 function formatBookID(book) {
   return book.title.split(" ").join("") + book.pages
 }
 
-generateTable()
+UIRenderer.generateTable()
 
 const openModalBtn = document.querySelector("#open-modal-btn")
 const addBookBtn = document.querySelector("#add-book-btn")
 const modal = document.querySelector("dialog")
 const closeModalBtn = document.querySelector("#close-modal-btn")
 
-function handleOpenModal(e) {
-  e.preventDefault();
-  form.reset()
-  modal.showModal();
-}
-
-openModalBtn.addEventListener("click", (e) => handleOpenModal(e))
-form.addEventListener("submit", (e) => addBookToLibrary(e))
-addBookBtn.addEventListener("click", (e) => handleAddBookBtn())
-closeModalBtn.addEventListener("click", (e) => handleCloseModal(e))
 
 
-function handleAddBookBtn() {
-  modal.close()
-  console.log("hola")
-}
 
-function handleCloseModal(e) {
-  e.preventDefault
-  modal.close()
-}
+openModalBtn.addEventListener("click", (e) => UIRenderer.handleOpenModal(e))
+form.addEventListener("submit", (e) => Library.addBookToLibrary(e))
+addBookBtn.addEventListener("click", (e) => UIRenderer.handleAddBookBtn())
+closeModalBtn.addEventListener("click", (e) => UIRenderer.handleCloseModal(e))
+
+
+
